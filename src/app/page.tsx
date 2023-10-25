1b4f4a4014a3ad466ef65a546e5328fb4efb8a96
Nashruddin Amin <nashruddin.amin@gmail.com>
2023-10-25 12:33:29 +0700

    Created basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
+'use client'
+
 export default function Home() {
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold tracking-tight">
-        Hello, World!
-      </h1>
+    <div className="flex">
+      <div className="shrink-0 hidden sticky w-[200px] top-0 left-0 h-screen bg-gray-100 sm:flex sm:flex-col">
+        <div className="grow">
+          <h2 className="text-xl font-bold tracking-tight">
+            Sidebar
+          </h2>
+        </div>
+        <span className="text-sm">
+          Bottom of sidebar
+        </span>
+      </div>
+
+      <div className="grow flex flex-col">
+        <div className="grow">
+          <h2 className="text-xl font-bold tracking-tight">
+            Main content
+          </h2>
+        </div>
+
+        <div className="sticky w-content bottom-0 py-4 bg-indigo-800">
+          <h2 className="text-xl tracking-tight text-white text-center">
+            Sticky panel
+          </h2>
+        </div>
+
+      </div>
     </div>
   )
 }
